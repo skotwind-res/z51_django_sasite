928b289b39af6c11deb77611853771094683fd2f
skotwind-res <skotwind.res.2@gmail.com>
2021-07-21 21:22:15 +0300

    Create model and some change in my app.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def some_shit(request):
+    return HttpResponse("<h2>My first page</h2>")
+
+def main(request):
+    return HttpResponse("<h2>Main page</h2>")
--- sasite/urls.py
+++ sasite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import some_shit, main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', some_shit),
+    path('', main)
 ]
