fa661c4bf95f37ca604f2119981707180f002f9d
skotwind-res <skotwind.res.2@gmail.com>
2021-07-26 21:55:40 +0300

    Change view model in admin.

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import CommonInfo
 
-admin.site.register(CommonInfo)
 # Register your models here.
+# admin.site.register(CommonInfo)
+
+class CommonInfoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content',)
+
+admin.site.register(CommonInfo, CommonInfoAdmin)
